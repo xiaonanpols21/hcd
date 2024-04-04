493ec722b441b33a4ac80b5908b236811ffb9ce5
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-04 20:44:41 +0200

    get right data after clicked

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,52 @@
 const categoryShirts = document.querySelector("#category-shirts");
+let dataPromise;
 
 // Fetch data
 async function getData() {
     const response = await fetch("../public/data/data.json"); 
     const data = await response.json();
-
-    showData(data);
-
+    return data;
 }
-getData();
+
+// Get data and resolve promise
+dataPromise = getData().then((data) => {
+    showData(data);
+    return data;
+});
 
 function showData(data) {
-    data.shirts.forEach(item => {
+    data.shirts.forEach( item => {
         const img = item.img;
         const description = item.description;
 
         const html = 
         `<article>
             <img src="${img}" alt="${description}">
-            <button onclick="addCombine()">Ik wil dit aan</button>
+            <button onclick="addCombine(${item.id})">Ik wil dit aan</button>
         </article>`;
 
         categoryShirts.insertAdjacentHTML("beforeend", html);
     });
 }
 
-function addCombine() {
+async function addCombine(currentShirtId) {
+    const data = await dataPromise;
+
+    // Get current shirt id
+    // Zie prompts: https://chemical-bunny-323.notion.site/HCD-Chat-gpt-Doc-76ba691317274604955fcc03b75bc8ea#41f9c46b5c1f4ed38eb30ad9b03e7306
+    console.log("Current shirt Id:", currentShirtId);
+    
+    console.log(data);
+
+    const broekenCombineData = [];
+    data.broeken.forEach(item => {
+        if (item.combine.includes(currentShirtId)) {
+            broekenCombineData.push(item);
+        }
+    });
+    console.log(broekenCombineData)
+    
+
     const combineSection = `
     <section class="combine">
         <h4>Bijhorende broeken</h4>
