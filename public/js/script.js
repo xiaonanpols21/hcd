2e6e625a845f506b38886a4094c99d3d388e9610
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-10 11:57:46 +0200

    hide local storage for now

--- public/js/script.js
+++ public/js/script.js
@@ -55,12 +55,12 @@ async function addPants(currentShirtId) {
     console.log(currentShirtId)
     this.currentShirtId = currentShirtId;
 
-    updateLocalStorage({
-        target: {
-            name: 'shirt',
-            value: currentShirtId
-        }
-    });
+    // updateLocalStorage({
+    //     target: {
+    //         name: 'shirt',
+    //         value: currentShirtId
+    //     }
+    // });
 
     combineBroekenSec.classList.remove("none");
     const data = await dataPromise;
@@ -75,8 +75,6 @@ async function addPants(currentShirtId) {
             broekenCombineData.push(item);
         }
     });
-
-    console.log(broekenCombineData)
     
     broekenCombineData.forEach( item => {
         const img = item.img;
@@ -112,12 +110,12 @@ async function addShoos(currentPantsId) {
     console.log(currentPantsId)
     this.currentPantsId = currentPantsId;
 
-    updateLocalStorage({
-        target: {
-            name: 'pants',
-            value: currentPantsId
-        }
-    });
+    // updateLocalStorage({
+    //     target: {
+    //         name: 'pants',
+    //         value: currentPantsId
+    //     }
+    // });
 
     combineSchoenenSec.classList.remove("none");
     const data = await dataPromise;
@@ -132,8 +130,6 @@ async function addShoos(currentPantsId) {
             schoenenCombineData.push(item);
         }
     });
-
-    console.log(schoenenCombineData)
     
     schoenenCombineData.forEach( item => {
         const img = item.img;
@@ -152,12 +148,12 @@ async function addShoos(currentPantsId) {
 async function showResults(currentShoosId) {
     console.log(currentShoosId);
 
-    updateLocalStorage({
-        target: {
-            name: 'shoes',
-            value: currentShoosId
-        }
-    });
+    // updateLocalStorage({
+    //     target: {
+    //         name: 'shoes',
+    //         value: currentShoosId
+    //     }
+    // });
     
     resultsSec.classList.remove("none");
     combineSchoenenSec.classList.add("none");
@@ -169,21 +165,21 @@ async function showResults(currentShoosId) {
 // Local storage
 // Bron: https://blog.logrocket.com/localstorage-javascript-complete-guide/
 // Zie prompts: https://chemical-bunny-323.notion.site/HCD-Chat-gpt-Doc-76ba691317274604955fcc03b75bc8ea#d07731522b274a09bad98712f9ce0252
-function updateLocalStorage(event) {
-    const chosenItems = JSON.parse(localStorage.getItem('chosenItems')) || {};
-    const itemType = event.target.name;
-    const itemId = event.target.value;
+// function updateLocalStorage(event) {
+//     const chosenItems = JSON.parse(localStorage.getItem('chosenItems')) || {};
+//     const itemType = event.target.name;
+//     const itemId = event.target.value;
 
-    if (itemType === 'shirt') {
-        chosenItems['currentShirtId'] = itemId;
-    } else if (itemType === 'pants') {
-        chosenItems['currentPantsId'] = itemId;
-    } else if (itemType === 'shoes') {
-        chosenItems['currentShoosId'] = itemId;
-    }
+//     if (itemType === 'shirt') {
+//         chosenItems['currentShirtId'] = itemId;
+//     } else if (itemType === 'pants') {
+//         chosenItems['currentPantsId'] = itemId;
+//     } else if (itemType === 'shoes') {
+//         chosenItems['currentShoosId'] = itemId;
+//     }
 
-    localStorage.setItem('chosenItems', JSON.stringify(chosenItems));
-    console.log(chosenItems);
-}
+//     localStorage.setItem('chosenItems', JSON.stringify(chosenItems));
+//     console.log(chosenItems);
+// }
 
 
