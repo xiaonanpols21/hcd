6f7007d6eb6d8fff9d9348868eb977caa6ba66e6
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-12 12:04:20 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,14 @@
 const links = document.querySelectorAll('a');
 
+async function getData() {
+    const response = await fetch("public/data/data.json");
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+const dataPromise = getData(); 
+
+
 // Local storage next page
 // Zie prompts: https://chemical-bunny-323.notion.site/HCD-Chat-gpt-Doc-76ba691317274604955fcc03b75bc8ea#3336b72f01f742a88d4b973cd75b1ba1
 function chooseCategory(event) {
