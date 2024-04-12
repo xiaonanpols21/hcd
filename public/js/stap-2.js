cf4d0f7bd1d0ad702b945335b13a9ba62d91bc00
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-12 12:22:02 +0200

    Update stap-2.js

--- public/js/stap-2.js
+++ public/js/stap-2.js
@@ -2,7 +2,6 @@ const mainUlEl = document.querySelector("main ul");
 
 function getChosenCategory() {
     const selectedItem1 = localStorage.getItem('selectedItem1');
-    const selectedCategory = localStorage.getItem('selectedCategory');
 
     if (selectedItem1) {
         const selectedItemArray = JSON.parse(selectedItem1);
@@ -25,22 +24,15 @@ const dataPromise = getData();
 
 // Show next category
 // Zie prompts: https://chemical-bunny-323.notion.site/HCD-Chat-gpt-Doc-76ba691317274604955fcc03b75bc8ea#cfcbd0cab1d64f2285f018a24560d9d4
-async function showNextCategoryItems(data, selectedItem1, selectedCategory) {
+async function showNextCategoryItems(data, selectedCategory) {
     const categories = Object.keys(data);
-    console.log("Categories:", categories);
-
-    console.log("Selected item:", selectedItem1);
-
     const nextIndex = (categories.indexOf(selectedCategory) + 1) % categories.length;
-    console.log("Next index:", nextIndex);
-
     const nextCategory = categories[nextIndex];
-    console.log("Next category:", nextCategory);
 
     const nextCategoryItems = data[nextCategory];
-    console.log("Next category items:", nextCategoryItems);
+    
 
-    console.log(`Je hebt ${selectedCategory} aangeklikt, de volgende categorie die dan komt is ${nextCategory}`);
+   
 
     return nextCategoryItems;
 }
@@ -78,7 +70,7 @@ async function initialize() {
     const selectedItem1 = await getChosenCategory(); // Retrieve selected category
     const selectedCategory = localStorage.getItem('selectedCategory'); // Retrieve selected category
     const data = await dataPromise; // Fetch data
-    const nextCategoryItems = await showNextCategoryItems(data, selectedItem1, selectedCategory);
+    const nextCategoryItems = await showNextCategoryItems(data, selectedCategory);
     showData(nextCategoryItems, selectedItem1);
 }
 initialize();
