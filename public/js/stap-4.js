df5deae6947a0d1e43bad00387948a5871018fc7
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-12 14:03:32 +0200

    dynamic done

--- public/js/stap-4.js
+++ public/js/stap-4.js
@@ -1,31 +1,32 @@
 const mainUlEl = document.querySelector("main ul");
 
+// Show results
+// Zie prompts: https://chemical-bunny-323.notion.site/HCD-Chat-gpt-Doc-76ba691317274604955fcc03b75bc8ea#b6c7e7cccdf94e899f6603c8abc96ea6
 function getChosenItems() {
     const selectedItemsArray = [];
 
-    // Iterate through local storage
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        // Check if key starts with 'selected' prefix
-        if (key.startsWith('selected')) {
-            const selectedItem = localStorage.getItem(key);
-            try {
-                // Try to parse as JSON
-                const parsedItem = JSON.parse(selectedItem);
-                selectedItemsArray.push(parsedItem);
-            } catch (error) {
-                // If parsing fails, add as string
-                selectedItemsArray.push(selectedItem);
-            }
-        }
+    const selectedItem1 = localStorage.getItem('selectedItem1');
+    const selectedItem2 = localStorage.getItem('selectedItem2');
+    const selectedItem3 = localStorage.getItem('selectedItem3');
+
+    // Push selected items into the selectedItemsArray if they are not null
+    if (selectedItem1) {
+        selectedItemsArray.push(JSON.parse(selectedItem1));
+    }
+    if (selectedItem2) {
+        selectedItemsArray.push(JSON.parse(selectedItem2));
+    }
+    if (selectedItem3) {
+        selectedItemsArray.push(JSON.parse(selectedItem3));
     }
 
     console.log(selectedItemsArray);
     return selectedItemsArray;
 }
 
+
 async function showData(selectedItemsArray) {
-    selectedItemsArray.slice(1).forEach(item => {
+    selectedItemsArray.forEach(item => {
         const img = item.img;
         const description = item.description;
 
@@ -40,8 +41,7 @@ async function showData(selectedItemsArray) {
     });
 }
 
-// Call getChosenItems to retrieve selected items
+
 const selectedItemsArray = getChosenItems();
 
-// Call showData with the retrieved selected items
 showData(selectedItemsArray);
